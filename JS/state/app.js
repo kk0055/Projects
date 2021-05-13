0b53831363d57b4e9a26cd1487dc2d716208e9fe
kk0055 <rtsw.lgy@gmail.com>
2021-05-13 22:13:33 +0800

    JS state

---
+++ JS/state/app.js
@@ -0,0 +1,63 @@
+const PageState = function() {
+  let currentState = new homeState(this);
+
+  this.init = function() {
+    this.change(new homeState);
+  }
+
+  this.change = function(state) {
+    correntState = state;
+  }
+};
+
+//Home
+const homeState = function(page) {
+  document.querySelector('#heading').textContent = null;
+  document.querySelector('#content').innerHTML = `
+    <div class="jumbotron">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+   </div>
+  `;
+};
+
+
+//About
+
+const aboutState = function(page) {
+  document.querySelector('#heading').textContent = 'About us';
+  document.querySelector('#content').innerHTML = `
+ <p>This is a about page</p>
+  `;
+};
+
+//Contact
+
+const contactState = function(page) {
+  document.querySelector('#heading').textContent = 'Contact us';
+  document.querySelector('#content').innerHTML = `
+  <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  `;
+};
+
+//
