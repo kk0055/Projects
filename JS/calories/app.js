2375346589a2c77bd214098b612b6dd6c79a5ae5
kk0055 <rtsw.lgy@gmail.com>
2021-05-14 12:04:20 +0800

    Item fetch from data

--- JS/calories/app.js
+++ JS/calories/app.js
@@ -19,18 +19,60 @@ const ItemCtrl = (function(){
     totalCalories: 0
   }
 
+ //Publicメソッド
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
   }
 })();
 
+
+
 //UI Controller
 const UICtrl = (function(){
   
+  //Publicメソッド
+  return {
+    populateItemList: function(items){
+      let html = '';
+
+      items.forEach(function(item){
+        html += `<li class="collection-item" id="items-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector('#item-list').innerHTML = html;
+    }
+  }
 })();
+
+
 //App Controller
 const App = (function(ItemCtrl, UICtrl){
+
+   //Publicメソッド
+  return {
+    init:function(){
+
+      //Itemsのfetch
+      const items = ItemCtrl.getItems();
+
+      //populate list with items
+      UICtrl.populateItemList(items);
+    }
+  }
   
 })(ItemCtrl, UICtrl);
+
+
+App.init()
+
