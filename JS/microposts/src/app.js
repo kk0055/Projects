a27769a011d5af863bd570049be5f6416bec90a6
kk0055 <rtsw.lgy@gmail.com>
2021-05-20 16:41:41 +0800

    posts delete

--- JS/microposts/src/app.js
+++ JS/microposts/src/app.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+//Listen for delete
+document.querySelector('#posts').addEventListener('click',deletePost);
 //Get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -32,6 +34,21 @@ function submitPost() {
     getPosts();
   })
   .catch(err => console.log(err));
-  
-  
 }
+
+  //Delete
+  function deletePost(e) {
+  e.preventDefault();
+  if(e.target.parentElement.classList.contains('delete')) {
+   const id = e.target.parentElement.dataset.id;
+   if(confirm('Are you sure?')){
+     http.delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert('Post removed', 'alert alert-success');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+   }
+  }
+ 
+  }
