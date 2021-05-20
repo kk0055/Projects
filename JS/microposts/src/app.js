5c5b22d4cc475f9885322e6e4ea02c93da7e7544
kk0055 <rtsw.lgy@gmail.com>
2021-05-20 15:46:53 +0800

    alert added

--- JS/microposts/src/app.js
+++ JS/microposts/src/app.js
@@ -27,6 +27,8 @@ function submitPost() {
  //create Post
  http.post('http://localhost:3000/posts',data)
   .then(data => {
+    ui.showAlert('Post added','alert alert-success');
+    ui.clearFields();
     getPosts();
   })
   .catch(err => console.log(err));
--- JS/microposts/src/ui.js
+++ JS/microposts/src/ui.js
@@ -31,6 +31,33 @@ class UI {
 
     this.post.innerHTML = output;
   }
+  showAlert(message, className) {
+  this.clearAlert();
+
+  const div = document.createElement('div');
+  div.className = className;
+  div.appendChild(document.createTextNode(message));
+  //Get parent
+  const container = document.querySelector('.postsContainer');
+  const posts = document.querySelector('#posts');
+  //Insert alert div
+  container.insertBefore(div, posts);
+  setTimeout(() => {
+    this.clearAlert();
+  },3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert){
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
 }
 
 export const ui = new UI();
