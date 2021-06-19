6dd322a711097352db3459ecd9697897a5302ef9
kk0055 <rtsw.lgy@gmail.com>
2021-06-19 14:38:51 +0800

    animated countdown  runAnimation function

--- Animated_countdowm/script.js
+++ Animated_countdowm/script.js
@@ -1,1 +1,24 @@
 const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const reply = document.querySelector('#reply')
+
+runAnimation()
+
+function runAnimation() {
+  nums.forEach((num,idx) => {
+    const nextToLast = nums.length - 1
+
+    num.addEventListener('animationend', (e) => {
+      if(e.animationName === 'goIn' && idx !== nextToLast) {
+         num.lassList.remove('in')
+         num.lassList.add('out')
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      }else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
