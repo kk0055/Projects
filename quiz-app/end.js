d436801254f9195acd4c4af1f13bbd7c3ada6232
kk0055 <rtsw.lgy@gmail.com>
2021-07-20 13:56:24 +0800

    Quiz app save high score in  localStorage

--- quiz-app/end.js
+++ quiz-app/end.js
@@ -3,6 +3,7 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+//highScoresがあればlocalStorageから取得。無い場合はからの配列を取得
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
@@ -15,6 +16,7 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+    
     const score = {
       score: mostRecentScore,
       name: username.value,
@@ -24,5 +26,5 @@ saveHighScore = (e) => {
   highScores.splice(5);
 
   localStorage.setItem('highScores', JSON.stringify(highScores));
-  window.location.assign('/');
+  window.location.assign('index.html');
 };
