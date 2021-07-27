14ffde8919eb8bd99e0dead4a3df7b1a97d213fb
kk0055 <rtsw.lgy@gmail.com>
2021-07-27 11:39:35 +0800

    require.js installed

--- API/script.js
+++ API/script.js
@@ -1,5 +1,23 @@
+var unirest = require('unirest');
+
 unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
   .header("X-RapidAPI-Key", "b982c2f91dmsh4f0d51d46c7722fp13fc13jsn83554faa35fe")
   .end(function (result) {
     console.log(result.status, result.headers, result.body);
   });
+
+//   var request = require('request');
+
+// var options = {
+//   method: 'get',
+//   url: 'https://wordsapiv1.p.mashape.com/words/soliloquy',
+//   headers: {
+//     'x-rapidapi-key': 'b982c2f91dmsh4f0d51d46c7722fp13fc13jsn83554faa35fe'
+//   }
+// };
+
+// request(options, function (error, response, body) {
+//     if (error) throw new Error(error);
+
+//     console.log(body);
+// });
