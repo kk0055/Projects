42001dc0a6178eb112900a1ff2d30791ffd092b8
kk0055 <rtsw.lgy@gmail.com>
2021-07-28 15:19:47 +0800

    Rapid API key

--- API/script.js
+++ API/script.js
@@ -1,7 +1,7 @@
 // var unirest = require('unirest');
 
 unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-  .header("X-RapidAPI-Key", "b982c2f91dmsh4f0d51d46c7722fp13fc13jsn83554faa35fe")
+  .header("X-RapidAPI-Key", "")
   .end(function (result) {
     console.log(result.status, result.headers, result.body);
   });
