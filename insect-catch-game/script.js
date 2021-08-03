f1d5e810795fa0964572102311c6b553a251c95f
kk0055 <rtsw.lgy@gmail.com>
2021-08-03 14:00:43 +0800

    insect catch game increaseScore function 追加

--- insect-catch-game/script.js
+++ insect-catch-game/script.js
@@ -64,5 +64,15 @@ function getRandomLocation() {
 }
 
 function catchInsect() {
-  console.log(123)
+  increaseScore()
+  this.classList.add('caught')
+  setTimeout(() => this.remove(), 200)
+}
+
+function increaseScore() {
+  score++
+  if(score > 19) {
+    message.classList.add('visible')
+  }
+  scoreEl.innerHTML = `Score: ${score}`
 }
