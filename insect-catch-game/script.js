8947010453d3e6a83950a30b92d7380b69de299b
kk0055 <rtsw.lgy@gmail.com>
2021-08-03 11:34:43 +0800

    insect catch game createInsect function　追加

--- insect-catch-game/script.js
+++ insect-catch-game/script.js
@@ -5,3 +5,28 @@ const game_container = document.getElementById('game-container')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+//画面の切り替え
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+//insectを選択
+choose_insect_btns.forEach(btn =>{
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img')
+    const src = img.getAttribute('alt')
+    selected_insect = {src, alt}
+    screens[1].classList.add('up')
+    setTimeout(createInsect, 1000)
+    startGame()
+  })
+})
+
+function createInsect() {
+  const insect = document.createElement('div')
+  insect.classList.add('insect')
+  const {x,y} = getRandomLocation()
+}
