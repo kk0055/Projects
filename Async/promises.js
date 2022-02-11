3c27a138fa1b5aad70bd4900b04fe7a35abdf2a7
kk0055 <rtsw.lgy@gmail.com>
2022-02-11 14:53:19 +0900

    Promise all

--- Async/promises.js
+++ Async/promises.js
@@ -36,8 +36,17 @@ function createPost(post) {
 
 }
 
-createPost({
-    title: "Post two",
-    body: "This is post two"
-  })
-  .then(getPosts);
+// createPost({
+//     title: "Post two",
+//     body: "This is post two"
+//   })
+//   .then(getPosts)
+//   .catch(error => console.log(error))
+
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, "goode bye"));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users/').then(res => res.json())
+
+Promise.all([promise1, promise2, promise3, promise4]).then((val) => console.log(val));
