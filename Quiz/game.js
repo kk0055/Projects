aa65fb0a089db46cad96a634e3045fba2999116a
kk0055 <rtsw.lgy@gmail.com>
2022-02-21 23:07:06 +0900

    counter added

--- Quiz/game.js
+++ Quiz/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -7,32 +9,30 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
+let questions = [{
+    question: 'Inside which HTML element do we put the JavaScript??',
+    choice1: '<script>',
+    choice2: '<javascript>',
+    choice3: '<js>',
+    choice4: '<scripting>',
+    answer: 1,
+  },
+  {
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3,
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4,
+  },
 ];
 
 //CONSTANTS
@@ -48,11 +48,12 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-  if(availableQuesions.length ===  0 || questionCounter >= MAX_QUESTIONS) {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
 
     return window.location.assign('./end.html');
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
   currentQuestion = availableQuesions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -60,7 +61,7 @@ getNewQuestion = () => {
   choices.forEach((choice) => {
     const number = choice.dataset['number'];
     choice.innerText = currentQuestion['choice' + number];
-  
+
   });
 
   availableQuesions.splice(questionIndex, 1)
@@ -69,13 +70,30 @@ getNewQuestion = () => {
 }
 choices.forEach((choice) => {
   choice.addEventListener('click', (e) => {
-      if (!acceptingAnswers) return;
+    if (!acceptingAnswers) return;
 
-      acceptingAnswers = false;
-      const selectedChoice = e.target;
-      const selectedAnswer = selectedChoice.dataset['number'];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
+    }, 1000)
+    console.log(classToApply)
+
   });
 });
 
-startGame ()
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame()
