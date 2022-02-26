deb45b2f81dff0f89710939e22e7ab4427637283
kk0055 <rtsw.lgy@gmail.com>
2022-02-26 14:05:24 +0900

    fetch questions from API

--- Quiz/game.js
+++ Quiz/game.js
@@ -12,21 +12,36 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('questions.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions;
-        startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    questions =
+      loadedQuestions.results.map((loadedQuestion) => {
+        const formattedQuestion = {
+          question: loadedQuestion.question
+        }
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+        answerChoices.splice(
+          formattedQuestion.answer - 1, 0,
+          loadedQuestion.correct_answer
+        );
+        answerChoices.forEach((choice, index) => {
+          formattedQuestion['choice' + (index + 1)] = choice;
+        })
+        return formattedQuestion;
+      })
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
   questionCounter = 0
