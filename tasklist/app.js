3ac11f9a797d205cdd00a33f2a8ebbeed91b90a3
kk0055 <rtsw.lgy@gmail.com>
2022-03-01 23:44:06 +0900

    save data into localStorage

--- tasklist/app.js
+++ tasklist/app.js
@@ -41,11 +41,27 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  storeTaskInLocalStorage(taskInput.value );
+
   // Clear input
   taskInput.value = '';
 
   e.preventDefault();
 }
+
+function  storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.push(task)
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
 
 function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
