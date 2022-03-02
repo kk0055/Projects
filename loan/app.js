c069e95581462e4a98a178e063188c5766a1b5fa
kk0055 <rtsw.lgy@gmail.com>
2022-03-02 22:47:09 +0900

    loan calculator done

--- loan/app.js
+++ loan/app.js
@@ -1,5 +1,14 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+ 
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e){
@@ -24,15 +33,20 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
+
 }
 
 // Show Error
 function showError(error){
+  document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
   // Create a div
   const errorDiv = document.createElement('div');
 
